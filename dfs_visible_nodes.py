66e57fdadcac32c678323427c46719c17d837ea3
renugopal <renugopal17@hotmail.com>
2025-03-02 00:48:27 +0000

    dfs visible nodes v2

--- dfs_visible_nodes.py
+++ dfs_visible_nodes.py
@@ -50,6 +50,42 @@ def find_visible_nodes(root)-> int:
     return count_visible
 
 
+def find_visible_nodes_v2(root)-> int:
+    """
+    Given binary tree
+    Count no of visible nodes
+    Visible - no nodes bigger than itself from root to itself
+    We keep state
+    Count increases if we encounter
+    DFS - from root to node, we keep track of max seen so far
+    if cur val > max seen so far 
+    then count ++
+    max seen so far updated
+
+    """
+    # use recursion
+    # can change to stack later if needed
+
+
+    def dfs(node, max_seen)->int:
+        if not node:
+            return 0
+        
+        count_visible = 0
+
+        if node.val >= max_seen:
+            count_visible += 1
+        
+        # print("came to ", node.val, " max =", max_seen, " count = ", count_visible)
+
+        count_visible += dfs(node.left, max(max_seen, node.val))
+        count_visible += dfs(node.right, max(max_seen, node.val))
+
+        return count_visible
+
+    return dfs(root, -math.inf)
+
+
 root = TreeNode(3)
 root.left = TreeNode(9)
 root.right = TreeNode(20, TreeNode(15), TreeNode(7))
